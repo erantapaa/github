13b8ab90d4a49b5683cbe567ddb0ea644f5116a5
Erik Rantapaa <erantapaa@gmail.com>
2013-10-27 22:44:53 -0500

    Augmented search sample.

---
+++ Github/Util.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Github.Util (
+  Github.GithubAuth(..),
+  githubCreds,
+  sleepSeconds,
+  today,
+  rateLimit,
+  handleRateLimit
+) where
+
+import qualified Github.Private as Github
+import qualified Github.Data as Github
+import qualified Data.ByteString as BS
+import System.Posix.Env.ByteString (getEnv)
+import qualified Data.Char as C
+import Data.Word (Word8)
+import Control.Concurrent (threadDelay)
+import Data.Time.Clock (getCurrentTime, UTCTime(..))
+import Text.Printf (printf)
+import Data.Time.LocalTime (utc,utcToLocalTime,localDay)
+import Data.Time.Calendar (toGregorian)
+import qualified Control.Exception as E
+import qualified Network.HTTP.Types as W
+import Data.Maybe (fromMaybe,listToMaybe)
+import Network.HTTP.Conduit (HttpException(..))
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+
+data RateLimit = RateLimit { rateLimitLimit :: Int, rateLimitRemaining :: Int, rateLimitReset :: Int } deriving (Show)
+
+-- | return credentials found in GITHUB_CREDENTIALS
+githubCreds :: IO (Maybe Github.GithubAuth)
+githubCreds = do
+  c <- getEnv "GITHUB_CREDENTIALS"
+  return $ case c of
+    Nothing -> Nothing
+    Just s  ->
+      let (user,rest) = BS.breakSubstring "," s
+          pw = BS.drop 1 rest
+       in Just $ Github.GithubBasicAuth user pw
+
+-- | sleep for a number of seconds
+sleepSeconds :: Int -> IO()
+sleepSeconds n = threadDelay (n*1000000)
+
+-- | return today (in UTC) formatted as YYYY-MM-DD
+today :: IO String
+today = do
+  now <- getCurrentTime
+  let day = localDay $ utcToLocalTime utc now
+      (y,m,d) = toGregorian day
+   in return $ printf "%d-%02d-%02d" y m d
+
+-- | perform an IO operation handling rate limit errors
+handleRateLimit io = do
+  result <- io
+  case result of
+    Left e  ->
+      case rateLimit e of
+        Nothing -> return result
+        Just rl -> do putStrLn $ "rate limited: " ++ show rl
+                      now <- getCurrentTime
+                      let diff = rateLimitReset rl - (toSecs now)
+                      putStrLn $ "need to sleep for " ++ show diff
+                      sleepSeconds diff
+                      handleRateLimit io
+    otherwise -> return result
+
+-- | return the rate limit details from an error
+rateLimit :: Github.Error -> Maybe RateLimit
+rateLimit e =
+  case e of
+    Github.HTTPConnectionError except ->
+      case E.fromException except of
+        Just (StatusCodeException (W.Status 403 _) headers _) -> Just $ RateLimit limit remaining reset
+          where limit     = findHeader headers "X-RateLimit-Limit"
+                remaining = findHeader headers "X-RateLimit-Remaining"
+                reset     = findHeader headers "X-RateLimit-Reset"
+        Nothing -> Nothing
+    otherwise -> Nothing
+
+findHeader hs key =
+  case lookup key hs of
+    Nothing -> 0
+    Just x  -> fromMaybe 0 (maybeRead (map toChar $ BS.unpack x))
+
+-- | Attempts to parse a value from the front of the string.
+maybeRead :: Read a => String -> Maybe a
+maybeRead = fmap fst . listToMaybe . reads
+
+toChar :: Word8 -> Char
+toChar = C.chr . fromIntegral
+
+toSecs :: UTCTime -> Int
+toSecs = round . utcTimeToPOSIXSeconds
+
--- samples/Search/SearchRepos.hs
+++ samples/Search/SearchRepos.hs
@@ -1,31 +1,67 @@
-
+{-# LANGUAGE OverloadedStrings #-}
 module SearchRepos where
 
+import Github.Util (today,githubCreds,handleRateLimit)
 import qualified Github.Search as Github
+import qualified Github.Data as Github
+import Control.Monad (forM,forM_)
 import Data.Maybe (fromMaybe)
-import Data.List (intercalate)
 
-import Data.Time.LocalTime (utc,utcToLocalTime,localDay)
-import Data.Time.Clock (getCurrentTime)
-import Data.Time.Calendar (toGregorian)
-import Text.Printf (printf)
-
-today :: IO String
-today = do
-  now <- getCurrentTime
-  let day = localDay $ utcToLocalTime utc now
-      (y,m,d) = toGregorian day
-   in return $ printf "%d-%02d-%02d" y m d
+import Data.List (intercalate,sortBy,groupBy)
+import GHC.Exts (sortWith,groupWith)
 
 main = do
+  results <- todaysUpdates
+  print $ length results
+
+main2 = do
+  results <- todaysUpdates
+  let names = map Github.repoName (uniqueRepos results)
+  print names
+
+main3 = do
+  results <- todaysUpdates
+  let repos = sortWith Github.repoPushedAt (uniqueRepos results)
+  forM_ repos (\r -> do
+    putStrLn $ formatRepo r
+    putStrLn ""
+    )
+
+-- | remove duplicates from a Repo list
+uniqueRepos :: [ Github.Repo ] -> [ Github.Repo ]
+uniqueRepos rs = 
+  let sorted = sortWith Github.repoHtmlUrl rs
+      grouped = groupWith Github.repoHtmlUrl sorted
+  in map head grouped
+
+todaysUpdates = do
   day <- today
-  putStrLn $ "today is " ++ day
-  result <- Github.searchRepos $ "q=a in%3Aname language%3Ahaskell pushed%3A>" ++ day ++ "&per_page=100"
-  putStrLn $ either (("Error: "++) . show) formatResult result 
+  auth <- githubCreds
+  results <- haskellUpdates auth day
+  return $ concat results
 
-formatResult r =(intercalate "\n\n" $ map formatRepo (Github.searchReposRepos r))
-  ++ "\n\nCount: " ++ (show $ Github.searchReposTotalCount r)
+test1 = haskellUpdates Nothing "2013-10-27"
 
+test2 = do
+  auth <- githubCreds
+  haskellUpdates auth "2013-10-27"
+
+haskellUpdates auth date = forM ['a'..'e'] (\letter -> do
+   results <- haskellUpdates' auth date letter
+   case results of
+     Left e  -> do putStrLn $ "Error for letter '" ++ [letter] ++ "': " ++ show e;
+                   return []
+     Right r -> do putStrLn $ "letter " ++ [letter] ++ " count: " ++ show n
+                   return $ Github.searchReposRepos r
+       where n = Github.searchReposTotalCount r
+   )
+
+haskellUpdates' :: Maybe Github.GithubAuth -> String -> Char -> IO (Either Github.Error Github.SearchReposResult)
+haskellUpdates' auth date letter = do
+  let query = "q=" ++ [letter] ++ " in%3Aname language%3Ahaskell pushed%3A>" ++ date ++ "&per_page=100"
+  handleRateLimit $ Github.searchRepos' auth query
+
+formatRepo :: Github.Repo -> String
 formatRepo r =
   let fields = [ ("Name", Github.repoName)
                  ,("URL",  Github.repoHtmlUrl)
@@ -34,9 +70,9 @@ formatRepo r =
                  ,("Pushed-At", formatMaybeDate . Github.repoPushedAt)
                ]
   in intercalate "\n" $ map fmt fields
-    where fmt (s,f) = fill 12 (s ++ ":") ++ " " ++ (f r)
-          orEmpty = maybe "" id
-          fill n s = s ++ (replicate n' ' ')
+    where fmt (s,f) = fill 12 (s ++ ":") ++ " " ++ f r
+          orEmpty = fromMaybe ""
+          fill n s = s ++ replicate n' ' '
             where n' = max 0 (n - length s) 
 
 formatMaybeDate = maybe "???" formatDate
