5f9f41b87d596108b3e1ce51101caa0d1635482c
Erik Rantapaa <erantapaa@gmail.com>
2013-10-29 11:16:38 -0500

    Remove per-letter queries.

--- samples/Search/HaskellRepos.hs
+++ samples/Search/HaskellRepos.hs
@@ -10,19 +10,18 @@ import GHC.Exts (sortWith)
 import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
 
-repoQuery q letters = forM letters (\letter -> do
-   results <- q letter
+repoQuery q = do
+   results <- q
    case results of
-     Left e  -> do putStrLn $ "Error for letter '" ++ [letter] ++ "': " ++ show e;
+     Left e  -> do putStrLn $ "Error: " ++ show e
                    return []
-     Right r -> do putStrLn $ "letter " ++ [letter] ++ " count: " ++ show n
+     Right r -> do putStrLn $ "Count: " ++ show n
                    return $ Github.searchReposRepos r
        where n = Github.searchReposTotalCount r
-   )
 
-languageQuery :: Maybe GithubAuth -> String -> String  -> UTCTime -> Char -> IO (Either Github.Error Github.SearchReposResult)
-languageQuery auth language field utcTime letter = 
-  let query = "q=" ++ [letter] ++ " in%3Aname language%3A" ++ language ++ " " ++ field ++ "%3A>" ++ date ++ "&per_page=100"
+languageQuery :: Maybe GithubAuth -> String -> String  -> UTCTime -> IO (Either Github.Error Github.SearchReposResult)
+languageQuery auth language field utcTime =
+  let query = "q=language%3A" ++ language ++ " " ++ field ++ "%3A>" ++ date ++ "&per_page=100"
       date = formatUTC utcTime
   in do
      -- putStrLn $ "=== query: " ++ query
@@ -66,9 +65,9 @@ main = do
   -- putStrLn $ "delta = " ++ show delta
   -- putStrLn $ "now   = " ++ show now
   -- putStrLn $ "t     = " ++ show t
-  let q = languageQuery auth "haskell" field t 
-  results <- repoQuery q ['a'..'z']
-  let repos = sortWith Github.repoPushedAt (uniqueRepos $ concat $ results)
+  let q = languageQuery auth "haskell" field t
+  results <- repoQuery q
+  let repos = sortWith Github.repoPushedAt results
   forM_ repos (\r -> do
     putStrLn $ formatRepo r
     putStrLn ""
